e3e083c5bb3349fbf12fb79efa86a93c412a29ee
kothasivaiah <kothasivaiah99@gmail.com>
2021-10-08 17:49:13 +0530

    eigth commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,70 @@
-import React, { useState } from 'react';
-import './App.css'
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './Data';
-const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+import React,{useState,useEffect} from 'react'
+// import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+// import { FaQuoteRight } from 'react-icons/fa';
+import data from './data'
 
-function App() {
-  const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState(allCategories);
+const App = () => {
+   const [people,setPeople] = useState(data)
+   const [index,setIndex] = useState(0)
 
-  const filterItems = (category) => {
-    if (category === 'all') {
-      setMenuItems(items);
-      return;
-    }
-    const newItems = items.filter((item) => item.category === category);
-    setMenuItems(newItems);
-  };
+   useEffect(()=>{
+     const lastIndex = people.length - 1
+       if(index < 0) {
+         setIndex(lastIndex)
+       }
+       if(index > lastIndex){
+         setIndex(0)
+       }
+   },[index,people])
+
+   useEffect(()=>{
+     let slider = setInterval(()=>{
+       setIndex(index + 1)
+     },5000)
+      return () =>{
+        clearInterval(slider)
+      }
+   },[index])
 
   return (
-    <main>
-      <section className="menu section">
-        <div className="title">
-          <h2>our menu</h2>
-          <div className="underline"></div>
-        </div>
-        <Categories categories={categories} filterItems={filterItems} />
-        <Menu items={menuItems} />
-      </section>
-    </main>
-  );
+    <section className="section">
+    <div className="title">
+      <h2>
+        <span>/</span>reviews
+      </h2>
+    </div>
+    <div className="section-center">
+        {people.map((person,personIndex)=>{
+          const {id,image,name,title,quote} = person;
+          let position = 'nextSlide'
+          if(personIndex === index){
+            position = 'activeSlide' 
+          }
+          if(personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+            ){
+              position = 'lastSlide'
+            }
+
+             return (
+               <article className={position} nkey={id}>
+                 <img src={image} alt={name} className="person-img" />
+                 <h4>{name}</h4>
+    <p className="title">{title}</p>
+    <p className="text">{quote}</p>
+
+               </article>
+             )
+        })}
+        <button className="prev" onClick={()=> setIndex(index - 1)}>
+        {/* <FiChevronLeft /> */}
+                  </button>
+                  <button className="next" onClick={()=> setIndex(index + 1)}>
+                  {/* <FiChevronRight /> */}
+                  </button>
+    </div>
+    </section>
+  )
 }
 
-export default App;
+export default App
